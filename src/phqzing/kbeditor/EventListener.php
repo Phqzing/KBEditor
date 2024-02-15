a8a40b92e3e15d9f181a56cff56b7684ed660583
Phqzing <discordnidj@gmail.com>
2024-02-15 16:38:26 +0800

    init

---
+++ src/phqzing/kbeditor/EventListener.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace phqzing\kbeditor;
+
+
+class EventListener implements Listener {
+    
+}
---
+++ src/phqzing/kbeditor/Loader.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace phqzing\kbeditor;
+
+
+use pocketmine\plugin\PluginBase;
+
+
+class Loader extends PluginBase {
+
+
+    public function onEnable():void
+    {
+
+    }
+
+    public function onDisable():void
+    {
+        
+    }
+}
