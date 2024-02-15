6b23855d969636ed2b6480a4f3728eba45726631
Phqzing <discordnidj@gmail.com>
2024-02-15 20:28:41 +0800

    final

--- src/phqzing/kbeditor/EventListener.php
+++ src/phqzing/kbeditor/EventListener.php
@@ -4,6 +4,69 @@
 namespace phqzing\kbeditor;
 
 
+use pocketmine\event\Listener;
+use pocketmine\event\entity\{EntityDamageEvent, EntityDamageByEntityEvent};
+use pocketmine\player\Player;
+use pocketmine\math\Vector3;
+
+
 class EventListener implements Listener {
-    
+ 
+
+    public function onDamage(EntityDamageEvent $ev):void
+    {
+        $player = $ev->getEntity();
+
+        if(Loader::$only_players)
+        {
+            if(!($player instanceof Player)) return;
+        }
+        if($ev->isCancelled()) return;
+
+        if($ev instanceof EntityDamageByEntityEvent)
+        {
+            $damager = $ev->getDamager();
+            if(Loader::$only_players)
+            {
+                if(!($damager instanceof Player)) return;
+            }
+
+            $world = $damager->getWorld()->getFolderName();
+            $kb = Loader::getKnockBack($world);
+            if(!is_null($kb))
+            {
+                $horizontal = $kb["horizontal"] ?? 0.4;
+                $vertical = $kb["vertical"] ?? 0.4;
+                $attack_speed = $kb["attack-speed"] ?? 10;
+
+                $deltaX = $player->getLocation()->x - $damager->getLocation()->x;
+                $deltaZ = $player->getLocation()->z - $damager->getLocation()->z;
+
+                $ev->setKnockback(0);
+                $ev->setAttackCooldown((int)$attack_speed);
+                $this->modifyKB($player, $deltaX, $deltaZ, $horizontal, $vertical, $vertical);
+            } else {
+                $ev->setAttackCooldown(10);
+            }
+        }
+    }
+
+
+    public function modifyKB(Player $player, float $deltaX, float $deltaZ, float $horizontal, float $vertical, ?float $verticalLimit = 0.4):void
+    {
+		$f = sqrt($deltaX * $deltaX + $deltaZ * $deltaZ);
+		if($f <= 0) return;
+        $f = 1 / $f;
+
+        $motionX = $player->getMotion()->x / 2;
+        $motionY = $player->getMotion()->y / 2;
+        $motionZ = $player->getMotion()->z / 2;
+        $motionX += $deltaX * $f * $horizontal * 2;
+        $motionY += $vertical * 2;
+        $motionZ += $deltaZ * $f * $horizontal * 2;
+
+        if($motionY > ($vertical * 2)) $motionY = $vertical * 2;
+
+        $player->setMotion(new Vector3($motionX, $motionY, $motionZ));
+    }
 }
--- src/phqzing/kbeditor/Loader.php
+++ src/phqzing/kbeditor/Loader.php
@@ -10,13 +10,30 @@ use pocketmine\plugin\PluginBase;
 class Loader extends PluginBase {
 
 
+    private static $kbData = [];
+    private static $generalKB = [];
+    public static $only_players = true;
+
+
     public function onEnable():void
     {
+        $this->saveDefaultConfig();
 
+        $config = $this->getConfig()->getAll();
+        self::$kbData = $config["world-kb"];
+        self::$generalKB = $config["general-kb"];
+        self::$only_players = $config["only-players"];
+
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
     }
 
-    public function onDisable():void
+
+    public static function getKnockBack(string $world):array|null
     {
-        
+        if(isset(self::$kbData[$world]))
+        {
+            return self::$kbData[$world];
+        }
+        return empty(self::$generalKB) ? null : self::$generalKB;
     }
 }
